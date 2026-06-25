12f72f0e5264ac0e4a45c95918d79f581e0615e2
TheVaniman <110702203+TheVaniman@users.noreply.github.com>
2026-06-24 22:00:12 -0400

    Revert "Clerk authentication addition and Posthog integration"

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -64,6 +64,7 @@ export default function App() {
                                 />
 
 
+                                <UpcomingSubscriptionsCard data={UPCOMING_SUBSCRIPTIONS[0]}/>
 
                             </View>
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,7 @@
 import { Stack, SplashScreen} from "expo-router";
 import {useFonts} from 'expo-font';
 import {useEffect} from 'react';
-import {ClerkProvider} from '@clerk/expo';
-import {tokenCache} from '@clerk/expo/token-cache';
-import {Slot} from 'expo-router';
 import '@/global.css'
-import { PostHogProvider } from 'posthog-react-native';
-
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-if (!publishableKey) {
-  throw new Error('Missing Clerk publishable key');
-}
-SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [fontsLoaded] = useFonts ({
     'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
@@ -29,15 +19,6 @@ export default function RootLayout() {
   }, [fontsLoaded])
 
   if(!fontsLoaded) return null;
-  return (
-    <PostHogProvider
-      apiKey={process.env.EXPO_PUBLIC_POSTHOG_API_KEY!}
-      options={{ host: process.env.EXPO_PUBLIC_POSTHOG_HOST }}
-    >
-      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-        <Stack screenOptions={{headerShown:false}}/>;
-      </ClerkProvider>
-    </PostHogProvider>
-  );
+  return <Stack screenOptions={{headerShown:false}}/>;
 }
 
