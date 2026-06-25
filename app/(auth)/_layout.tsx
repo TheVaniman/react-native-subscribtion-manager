8767db925cc1a51ea73792733f8913b74013987e
TheVaniman <grantgeovany5@gmail.com>
2026-06-24 20:15:00 -0400

    Clerk authentication addition and Posthog integration

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,10 @@
-import { Stack } from "expo-router";
+import { Stack, Redirect } from "expo-router";
+import {useAuth} from "@clerk/expo";
 
 import '@/global.css'
 export default function RootLayout() {
+    const {isSignedIn, isLoaded} = useAuth();
+    if(!isLoaded) return null;
+    if(isSignedIn) return <Redirect href={"/"}/>;
     return <Stack screenOptions={{headerShown:false}}/>;
 }
